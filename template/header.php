88ed0482c512a01d525326dc5fb66b80f4c3a010
shofariaa <shofashofariah87@gmail.com>
2025-05-23 00:21:45 +0700

    menambahkan view data barang dan delete barang

--- template/header.php
+++ template/header.php
@@ -9,13 +9,24 @@
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet"
     href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+
   <!-- Font Awesome Icons -->
   <link rel="stylesheet" href="<?= $main_url; ?>assets/AdminLTE/plugins/fontawesome-free/css/all.min.css">
+
   <!-- IonIcons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+
+  <!-- jQuery -->
+<script src="<?= $main_url; ?>assets/AdminLTE/plugins/jquery/jquery.min.js"></script>
+
+  <!-- SweetAlert2 -->
+  <link rel="stylesheet" href="<?= $main_url; ?>assets/AdminLTE/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css">
+  <script src="<?= $main_url; ?>assets/AdminLTE/plugins/sweetalert2/sweetalert2.min.js"></script>
+
   <!-- DataTables -->
   <link rel="stylesheet" href="<?= $main_url; ?>assets/AdminLTE/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
   <link rel="stylesheet" href="<?= $main_url; ?>assets/AdminLTE/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  
   <!-- Theme style -->
   <link rel="stylesheet" href="<?= $main_url; ?>assets/AdminLTE/dist/css/adminlte.min.css">
 </head>
